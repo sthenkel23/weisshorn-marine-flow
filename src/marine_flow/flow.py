a5a4a3e96cce5850aae807717908a06d35acad78
Steffen Henkelmann <coder.henkelmann@gmail.com>
2022-09-12 08:59:16 +0200

    add parallel tasks

--- src/marine_flow/flow.py
+++ src/marine_flow/flow.py
@@ -1,14 +1,15 @@
 from prefect import flow, task
 
+
 @task
 def say_hello():
-	print("Hello, World! I'm Marvin!")
+    print("Hello, World! I'm Marvin!")
 
 
 @flow(name="marine-flow")
 def marine_flow():
-	say_hello()
+    say_hello()
 
-if __name__ == '__main__':
-    marine_flow() # "Hello, World! I'm Marvin!"
 
+if __name__ == "__main__":
+    marine_flow()  # "Hello, World! I'm Marvin!"
