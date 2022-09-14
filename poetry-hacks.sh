f75d674d22e1c54360109b9287dca8a3eb8b892d
Steffen Henkelmann <coder.henkelmann@gmail.com>
2022-09-14 16:27:15 +0200

    replacing nose2 w/ pytest

--- poetry-hacks.sh
+++ poetry-hacks.sh
@@ -3,6 +3,6 @@
 PACKAGE_NAME=marine-flow
 
 poetry new --src $PACKAGE_NAME
-poetry add --group dev darglint flake8 black isort mypi nose2 nose2-cov pylint pydocstyle safety
+poetry add --group dev darglint flake8 black isort mypi pytest pytest-cov pylint pydocstyle safety
 
 poetry install
