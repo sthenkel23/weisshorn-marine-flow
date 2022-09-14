bd71092591a4c12895ebca014622eb10828eb468
Steffen Henkelmann <coder.henkelmann@gmail.com>
2022-09-14 17:03:34 +0200

    up

--- src/marine_flow/flow.py
+++ src/marine_flow/flow.py
@@ -1,15 +1,29 @@
+import sys
+import requests
 from prefect import flow, task
 
 
 @task
-def say_hello():
-    print("Hello, World! I'm Marvin!")
+def call_api(url):
+    response = requests.get(url)
+    print(response.status_code)
+    return response.json()
+
+
+@task
+def parse_fact(response):
+    fact = response["fact"]
+    print(fact)
+    return fact
 
 
 @flow(name="marine-flow")
-def marine_flow():
-    say_hello()
+def marine_flow(url):
+    fact_json = call_api(url)
+    fact_text = parse_fact(fact_json)
+    return fact_text
 
 
 if __name__ == "__main__":
-    marine_flow()  # "Hello, World! I'm Marvin!"
+    url = sys.argv[1]
+    marine_flow(url)  # "Hello, World! I'm Marvin!"
