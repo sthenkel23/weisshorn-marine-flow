b552f7a46ccff1b89d13991f51d9bb7315a9555d
Steffen Henkelmann <coder.henkelmann@gmail.com>
2022-09-15 20:23:39 +0200

    up

--- flows/flow.py
+++ flows/flow.py
@@ -15,7 +15,7 @@ def get_price(response):
     return r["amount"]
 
 
-@flow(name="weisshorn-marine-flow-18sep")
+@flow(name="weisshorn-marine-flow-18")
 def marine_flow(url):
     r = call_api(url)
     price = get_price(r)
