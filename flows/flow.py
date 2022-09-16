46d991101612dc2593a61f10d3d03f05eee24166
Steffen Henkelmann <coder.henkelmann@gmail.com>
2022-09-16 03:49:08 +0200

    up

--- flows/flow.py
+++ flows/flow.py
@@ -1,16 +1,11 @@
 import sys
 import requests
-from prefect import Flow, task
-from prefect.storage import GitHub
-from prefect.run_configs import LocalRun
+from prefect import flow, task
+from prefect.filesystems import GitHub
 
-
-FLOW_NAME = "weisshorn-marine-flow-20"
-STORAGE = GitHub(
-    repo="sthenkel23/weisshorn-marine-flow",
-    path=f"flows/flow.py",
-    access_token_secret="GITHUB_ACCESS_TOKEN",  # required with private repositories
-)
+block = GitHub(repository="https://github.com/sthenkel23/weisshorn-marine-flow")
+block.get_directory("deployment") # specify a subfolder of repo
+block.save("github-storage-dev")
 
 
 @task
@@ -19,7 +14,6 @@ def call_api(url):
     print(response.status_code)
     return response.json()
 
-
 @task
 def get_price(response):
     r = response["data"]
@@ -27,21 +21,14 @@ def get_price(response):
     return r["amount"]
 
 
-# @flow(name="weisshorn-marine-flow-20")
-# def marine_flow(url):
-with Flow(
-    FLOW_NAME,
-    storage=STORAGE,
-    run_config=LocalRun(
-        labels=["dev"],
-    ),
-) as marine_flow:
+@flow(name="weisshorn-marine-flow-20")
+def marine_flow(url):
     r = call_api(url)
     price = get_price(r)
-    # return price
+    return price
 
 
 if __name__ == "__main__":
-    URL = sys.argv[1]  # https://api.coinbase.com/v2/prices/ETH-USD/spot
+    URL = sys.argv[1]   # https://api.coinbase.com/v2/prices/ETH-USD/spot
     while True:
         marine_flow(URL)
