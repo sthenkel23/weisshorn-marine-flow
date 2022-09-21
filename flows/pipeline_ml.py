f46b8b56fafcb8ecdc503bfe3d3e9ab56173d0ed
Steffen Henkelmann <coder.henkelmann@gmail.com>
2022-09-21 23:24:30 +0200

    iup

--- flows/pipeline_ml.py
+++ flows/pipeline_ml.py
@@ -25,12 +25,24 @@ def transform(df) -> pd.DataFrame:
     print(df.dtypes)
     return df
 
+@task
+def split(df) -> pd.DataFrame:
+    tmin = df.timestamp.min()
+    tmax = df.timestamp.max()
+    split_num = int(len(df.index)*0.8)
+    
+    df_train = df[:split_num]
+    df_val = df[split_num:len(df.index)]
+    
+    return df_train, df_val
 
 @flow(name=f"{FLOW_NAME}")
 def ml_training_flow():
     df = retrieve_data(document="prices")
     df = transform(df)
-    print(df)
+    df_train, df_val = split(df)
+    
+    print(df_train, df_val)
     return df
 
 
